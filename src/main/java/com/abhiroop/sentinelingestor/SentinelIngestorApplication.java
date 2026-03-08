d8840089dd36c77512cbe5d3e27be20ed85e7bba
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-08 16:53:10 +0530

    initial

---
+++ src/main/java/com/abhiroop/sentinelingestor/SentinelIngestorApplication.java
@@ -0,0 +1,11 @@
+package com.abhiroop.sentinelingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelIngestorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelIngestorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/abhiroop/sentinelingestor/SentinelIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiroop.sentinelingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelIngestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
