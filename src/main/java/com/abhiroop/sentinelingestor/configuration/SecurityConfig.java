ae6c4ba1603751019a9c2192ea9f6004b8cd95c6
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-08 18:00:13 +0530

    ADD: allow /event for all and remove CORS as its a server to server connection in cloud

--- src/main/java/com/abhiroop/sentinelingestor/configuration/SecurityConfig.java
+++ src/main/java/com/abhiroop/sentinelingestor/configuration/SecurityConfig.java
@@ -7,11 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,28 +20,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/health/liveness").permitAll()
                         .requestMatchers("/actuator/health/readiness").permitAll()
+                        .requestMatchers("/event").permitAll()
                         .anyRequest().authenticated()
                 );
 
         return http.build();
-    }
-
-    //TODO: have to configure this
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final var corsConfig = new CorsConfiguration();
-
-        corsConfig.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "https://sentinel-25902.web.app/",
-                "https://sentinel-25902.firebaseapp.com/"
-        ));
-
-        corsConfig.setAllowedMethods(List.of("GET"));
-
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-        return source;
     }
 }
