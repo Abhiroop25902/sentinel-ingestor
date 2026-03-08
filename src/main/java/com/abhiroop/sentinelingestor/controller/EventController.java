69faaf51a1732e2c77fb38ae4f8a590195742e85
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-08 18:01:07 +0530

    ADD: /event controller

---
+++ src/main/java/com/abhiroop/sentinelingestor/controller/EventController.java
@@ -0,0 +1,23 @@
+package com.abhiroop.sentinelingestor.controller;
+
+import io.cloudevents.CloudEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+@Slf4j
+@RestController
+public class EventController {
+    @PostMapping("/event")
+    public ResponseEntity<CloudEvent> createEvent(@RequestBody CloudEvent event) {
+        log.debug("Event Id: {}", event.getId());
+        log.debug(new String(Objects.requireNonNull(event.getData()).toBytes(), StandardCharsets.UTF_8));
+
+        return ResponseEntity.ok().body(event);
+    }
+}
