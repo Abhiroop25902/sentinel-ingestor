e1bd2762e0239c72128a6ac74323df47c7afeefb
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-11 02:21:43 +0530

    temp_fix: google cloud logs is giving "PubSubMessageData[type=null, data=null]" hence adding direct string to debug

--- src/main/java/com/abhiroop/sentinelingestor/controller/EventController.java
+++ src/main/java/com/abhiroop/sentinelingestor/controller/EventController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 @Slf4j
@@ -25,12 +26,17 @@ public class EventController {
     public ResponseEntity<CloudEvent> createEvent(@RequestBody CloudEvent event) throws IOException {
         log.info("Event Id: {}", event.getId());
 
+        final var bytes = Objects.requireNonNull(event.getData()).toBytes();
+        final var jsonString = new String(bytes, StandardCharsets.UTF_8);
+
+        log.info("Event Data String: {}", jsonString);
+
         final PubSubMessageData<LoginHistoryDto> pubSubMessageData = objectMapper.readValue(
-                Objects.requireNonNull(event.getData()).toBytes(),
+                bytes,
                 new TypeReference<>() {
                 }
         );
-        
+
         log.info("Event Data: {}", pubSubMessageData);
 
         return ResponseEntity.ok().body(event);
