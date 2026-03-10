79514c770cedfa0ef3f270f67565e276ee6fe7c1
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-11 02:00:47 +0530

    add: convert the byte data to PubSubMessageData<LoginHistoryDto> and then log info the pubSubMessageData

--- src/main/java/com/abhiroop/sentinelingestor/controller/EventController.java
+++ src/main/java/com/abhiroop/sentinelingestor/controller/EventController.java
@@ -1,22 +1,37 @@
 package com.abhiroop.sentinelingestor.controller;
 
+import com.abhiroop.sentinelingestor.dto.LoginHistoryDto;
+import com.abhiroop.sentinelingestor.dto.PubSubMessageData;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cloudevents.CloudEvent;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.util.Objects;
 
 @Slf4j
 @RestController
+@AllArgsConstructor
 public class EventController {
+    private final ObjectMapper objectMapper;
+
     @PostMapping("/event")
-    public ResponseEntity<CloudEvent> createEvent(@RequestBody CloudEvent event) {
+    public ResponseEntity<CloudEvent> createEvent(@RequestBody CloudEvent event) throws IOException {
         log.info("Event Id: {}", event.getId());
-        log.info(new String(Objects.requireNonNull(event.getData()).toBytes(), StandardCharsets.UTF_8));
+
+        final PubSubMessageData<LoginHistoryDto> pubSubMessageData = objectMapper.readValue(
+                Objects.requireNonNull(event.getData()).toBytes(),
+                new TypeReference<>() {
+                }
+        );
+        
+        log.info("Event Data: {}", pubSubMessageData);
 
         return ResponseEntity.ok().body(event);
     }
