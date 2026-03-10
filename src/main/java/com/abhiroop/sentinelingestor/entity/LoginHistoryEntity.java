c8e0a5533f951dca07f30c90b70db72752961f22
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-11 01:56:12 +0530

    add: LoginHistoryEntity (for firestore)

---
+++ src/main/java/com/abhiroop/sentinelingestor/entity/LoginHistoryEntity.java
@@ -0,0 +1,17 @@
+package com.abhiroop.sentinelingestor.entity;
+
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.cloud.spring.data.firestore.Document;
+import lombok.Builder;
+
+@Builder(toBuilder = true)
+@Document(collectionName = "login_history")
+public record LoginHistoryEntity(
+        @DocumentId
+        String id,
+        String email,
+        boolean success,
+        Timestamp timestamp
+) {
+}
