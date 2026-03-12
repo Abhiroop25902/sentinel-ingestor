a0b3445cc3edb1619161a19526454da224771213
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-12 15:28:14 +0530

    add: process now uses ingestorConfigService to get firestore config to dynamically switch off/on certain tasks, this will reduce future "small change and wait for deploy" time waste

--- src/main/java/com/abhiroop/sentinelingestor/core/eventprocessor/LoginHistoryEventProcessor.java
+++ src/main/java/com/abhiroop/sentinelingestor/core/eventprocessor/LoginHistoryEventProcessor.java
@@ -3,6 +3,9 @@ package com.abhiroop.sentinelingestor.core.eventprocessor;
 import com.abhiroop.sentinelingestor.dto.LoginHistoryDto;
 import com.abhiroop.sentinelingestor.entity.LoginHistoryEntity;
 import com.abhiroop.sentinelingestor.mapper.LoginHistoryMapper;
+import com.abhiroop.sentinelingestor.repository.LoginHistoryRepository;
+import com.abhiroop.sentinelingestor.service.IngesterConfigServiceKey;
+import com.abhiroop.sentinelingestor.service.IngestorConfigService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +20,8 @@ class LoginHistoryEventProcessor implements EventProcessorStrategy {
 
     private final ObjectMapper objectMapper;
     private final LoginHistoryMapper loginHistoryMapper;
-//    private final LoginHistoryRepository loginHistoryRepository;
+    private final IngestorConfigService ingestorConfigService;
+    private final LoginHistoryRepository loginHistoryRepository;
 
     @Override
     public String getStrategyKey() {
@@ -27,13 +31,24 @@ class LoginHistoryEventProcessor implements EventProcessorStrategy {
     @Override
     public Mono<LoginHistoryEntity> process(JsonNode dataNode) {
         return Mono.fromCallable(() -> objectMapper.treeToValue(dataNode, LoginHistoryDto.class))
-                //TODO: remove this doOnNext after debug
-                .doOnNext(loginHistoryDto -> log.info("loginHistoryDto: {}", loginHistoryDto))
                 .doOnError(e -> log.error("objectMapper.treeToValue error", e))
+                .doOnNext(loginHistoryDto -> {
+                    if (ingestorConfigService.getConfig(IngesterConfigServiceKey.PRINT_LOG, false, Boolean.class)) {
+                        log.info("loginHistoryDto: {}", loginHistoryDto);
+                    }
+                })
                 .map(loginHistoryMapper::toEntity)
-                //TODO: remove this doOnNext after debug
-                .doOnNext(loginHistoryEntity -> log.info("loginHistoryEntity: {}", loginHistoryEntity));
-
-//                .flatMap(loginHistoryRepository::save);
+                .doOnNext(loginHistoryEntity -> {
+                    if (ingestorConfigService.getConfig(IngesterConfigServiceKey.PRINT_LOG, false, Boolean.class)) {
+                        log.info("loginHistoryEntity: {}", loginHistoryEntity);
+                    }
+                })
+                .flatMap(loginHistoryEntity -> {
+                    if (ingestorConfigService.getConfig(IngesterConfigServiceKey.SAVE_TO_DB, false, Boolean.class)) {
+                        return loginHistoryRepository.save(loginHistoryEntity);
+                    }
+                    return Mono.just(loginHistoryEntity);
+                })
+                .doOnError(e -> log.error("loginHistoryRepository.saveToDb error", e));
     }
 }
